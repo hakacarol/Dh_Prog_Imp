8e486751d4a0cf6067f78b2352c51c087e70171e
hakacarol <haka.carolina@gmail.com>
2021-07-19 19:20:46 -0300

    atualizando json

--- Aula_14_json/.json.js
+++ Aula_14_json/.json.js
@@ -8,9 +8,48 @@ muito similar ao objeto literal que já conhecemos.
 
 Objeto Literal
 
-Admite aspas simples e duplas. 
-As chaves do objeto não usam aspas.
-Podemos escrever métodos sem problemas.
-Recomenda-se colocar uma vírgula na última propriedade.
+- Admite aspas simples e duplas. 
+- As chaves do objeto não usam aspas.
+- Podemos escrever métodos sem problemas.
+- Recomenda-se colocar uma vírgula na última propriedade.
 
+JSON
+
+- Apenas aspas duplas podem ser usadas.
+- As chaves estão entre aspas.
+- Não suporta métodos, apenas propriedades e valores.
+- Não podemos colocar uma vírgula no último elemento.
+
+JSON.parse()
+
+Converte um texto com formato JSON ao tipo de dado equivalente em JavaScript.
+● Recebe uma string de texto com formato JSON;
+● Devolve o mesmo dado que recebeu em formato JavaScript.
 */
+
+let dadosJson = '{"cidade": "São Paulo", "bairro": "Morumbi"}';
+let dadosConvertidos = JSON.parse(dadosJson);
+
+console.log(dadosConvertidos); // Será visto no console um objeto literal / { cidade: 'São Paulo', bairro: 'Morumbi' }
+
+/*
+JSON.stringify()
+
+Converte um tipo de dado de JavaScript em um texto em formato JSON.
+● Recebe um tipo de dado de JavaScript;
+● Devolve uma string de texto com formato JSON.
+*/
+
+let objetoLiteral = { nome: 'Carla', pais: 'Brasil' };
+let dadosConvertidos1 = JSON.stringify(objetoLiteral);
+
+console.log(dadosConvertidos1); // Será visto no console os dados em uma string do tipo JSON / '{ "nome": "Carla", "pais": "Brasil" }'
+
+/*
+.readFileSync()
+.writeFileSync()
+.appendFileSync()
+
+* estudar!!!*
+*/
+
---
+++ Aula_14_json/ex_microdesafio_estruturando_json.js
@@ -0,0 +1,18 @@
+/* Exercício “Estruturando JSON”
+
+Objetivo
+
+Entender como estruturar informações em JSON e como utilizar seus métodos.
+
+Micro desafios
+
+Crie um objeto, contendo int, string, object e array em seu conteúdo. Após isso transforme o array em JSON. Exiba o valor do JSON, copie e valide no site JSON Formatter & Validator
+
+Pegue o JSON validado na etapa anterior, e o atribua a uma string. Após isso o transforme em um objeto e acesse alguns de seus valores e os exiba.
+*/
+
+// let usuario = {
+//     nome: "Maria da Silva",
+//     idade: 30,
+//     notas: [7, 8.5, 8, 10]
+// }
